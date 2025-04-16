0c0c673db80b28343d7b747efba41febde5bbf28
y_kimura <y_kimura@LAPTOP-V5GJUHSS>
2025-04-16 11:37:17 +0900

    Subject.java SubjectDao.java作った

--- src/bean/Subject.java
+++ src/bean/Subject.java
@@ -1,5 +1,28 @@
 package bean;
 
-public class Subject {
+public class Subject implements java.io.Serializable {
 
+	private String schoolCd;
+	private String cd;
+	private String name;
+
+	public String getSchoolCd() {
+		return schoolCd;
+	}
+	public String getCd() {
+		return cd;
+	}
+	public String getName() {
+		return name;
+	}
+
+	public void setSchoolCd(String schoolCd) {
+		this.schoolCd=schoolCd;
+	}
+	public void setCd(String cd) {
+		this.cd=cd;
+	}
+	public void setName(String name) {
+		this.name=name;
+	}
 }
