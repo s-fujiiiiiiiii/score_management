471295c501738aa838d0dada5b47323a88ea1ee3
y_kimura <y_kimura@LAPTOP-V5GJUHSS>
2025-04-21 12:43:33 +0900

    JSPへのデータの受け渡し問題な気がするのでどうにか対処法を見つけないといけない

--- src/main/SubjectDeleteAction.java
+++ src/main/SubjectDeleteAction.java
@@ -3,13 +3,14 @@ package main;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import dao.SubjectDao;
 
-//@WebServlet("/SubjectDeleteAction")
+@WebServlet("/SubjectDeleteAction")
 public class SubjectDeleteAction extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         try {
