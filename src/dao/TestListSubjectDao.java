3952b6e988841d9455d5065b4ff89bd37e9698bf
y_kimura <y_kimura@LAPTOP-V5GJUHSS>
2025-04-23 14:12:12 +0900

    成績関連

--- src/dao/TestListSubjectDao.java
+++ src/dao/TestListSubjectDao.java
@@ -7,26 +7,24 @@ import java.util.HashMap;
 
 import bean.TestListSubject;
 
-public class TestListSubjectDao extends Dao { // Dao を継承
+public class TestListSubjectDao extends Dao {
     public TestListSubject findBySubjectCd(String subjectCd) throws Exception {
         Connection con = getConnection();
         String sql = "SELECT STUDENT_NO, POINT FROM TEST WHERE SUBJECT_CD = ?";
         TestListSubject subject = new TestListSubject();
         subject.setSubjectCd(subjectCd);
-        subject.setPoints(new HashMap<>());
+        subject.setPoints(new HashMap<>()); // Map の初期化
 
         try (PreparedStatement stmt = con.prepareStatement(sql)) {
             stmt.setString(1, subjectCd);
             ResultSet rs = stmt.executeQuery();
 
             while (rs.next()) {
-                subject.getPoints().put(rs.getString("STUDENT_NO"), rs.getInt("POINT"));
+                // 学生番号を Integer に変換して挿入
+                subject.getPoints().put(Integer.parseInt(rs.getString("STUDENT_NO")), rs.getInt("POINT"));
             }
-
-            rs.close();
-            stmt.close();
-            con.close();
         }
+
         return subject;
     }
 }
